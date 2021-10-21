793b83b80e19f5c4052389c91680573e823bcba4
Aar Kaiser <studies@mohussein.com>
2021-10-20 22:17:47 -0400

    updating directories

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
-const generateHTML = require("./utils/generateHTML");
+const generateHTML = require("./src/generateHTML");
 const Manager = require("./lib/Manager");
 const Engineer = require("./lib/Engineer");
 const Intern = require("./lib/Intern");
