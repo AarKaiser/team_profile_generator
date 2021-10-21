9665b821224d47e09531a40327bed7f3ce341496
Aar Kaiser <studies@mohussein.com>
2021-10-20 23:21:06 -0400

    reuploading to fix dist folder

--- _tests_/Engineer.test.js
+++
@@ -1,8 +0,0 @@
-const Engineer = require("../lib/Engineer");
-
-describe("The Engineer Object", () => {
-    it("should get me the Engineer's Name back if I run getName()", ()=> {
-        const testObj = new Engineer ("John");
-        expect(testObj.getName()).toBe("John")
-    })
-})
